1119df34a2c7ad2fbf35656be04ff992c9d0748b
Sudam Shaluka <rkshaluka@gmail.com>
2024-04-06 16:15:23 +0530

    Initial Commit

---
+++ ems-backend/src/main/java/com/ijse/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.ijse.ems.mapper;
+
+import com.ijse.ems.dto.EmployeeDto;
+import com.ijse.ems.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapEmployeeDto (Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastname(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
---
+++ ems-backend/src/test/java/com/ijse/ems/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
